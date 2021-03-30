77abeeea16ed26d7655cce6622f19d827a148890
Robebou <73123940+Robebou@users.noreply.github.com>
2021-03-30 17:38:00 +0100

    ajout ejs
    Co-Authored-By: Touameme <73127345+TOUAMEME@users.noreply.github.com>
    Co-Authored-By: BlanchardNicolas <74158844+BlanchardNicolas@users.noreply.github.com>
    Co-Authored-By: GL-cod <73124938+GL-cod@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const http = require('http');
+const express = require('express');
+const { response } = require('express');
+var app = express()
+var routes = require("./routes/routes")
 
-const requestListener = function (req, res) {
-  res.end('Test');
-  console.log("Test")
-}
+app.use(express.static('views'))
+app.use('views', express.static(__dirname + 'views'))
 
-const server = http.createServer(requestListener);
-server.listen(8080);
+app.set('views','./views')
+app.set('view engine','ejs')
+
+app.use(routes)
+const PORT = 8080;
+const APIKEY = '9ea3fe10f4f94f70b6169e29f0f576d6';
+
+app.get('/', (req, res) => {
+  res.render('index',{}) 
+})
+
+app.listen(PORT, () => {
+  console.log("server started on port %d",PORT);
+})
